ce00b579a6396c6b9f0b1adfa742fedc05ab9d2d
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-22 20:31:28 -0300

    chore: create a users service.

---
+++ src/services/users.service.ts
@@ -0,0 +1,29 @@
+import Users from "../models/users";
+import UsersRepository from "../repository/users.repository";
+
+export default class UsersService {
+    private repository: UsersRepository;
+
+    constructor() {
+        this.repository = new UsersRepository();
+    }
+
+    public show = async (id: number): Promise<Users> => this.repository.show(id);
+
+    public store = async(data: Users): Promise<Users> => this.repository.store(data);
+
+    public removeData = async (id: number): Promise<string> => {
+        const obj = await this.show(id);
+        if (obj) {
+            await this.repository.delete(obj.id);
+            return 'user deleted';
+        }
+        return 'user not found.'
+    }
+
+    public updateData = async(data: Users): Promise<any> =>{
+        const obj = await this.repository.updateData(data);
+        console.log(obj);
+        return obj;
+    }
+}
