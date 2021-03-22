fa228f989b3c81a8a653bffe615c73d7f44812b1
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-22 20:31:13 -0300

    chore: create a users repository

---
+++ src/repository/users.repository.ts
@@ -0,0 +1,19 @@
+import { DeleteResult, getRepository, Repository, UpdateResult } from "typeorm";
+import Users from "../models/users";
+
+export default class UsersRepository extends Repository<Users>{
+    private repository: Repository<Users>;
+
+    constructor() {
+        super();
+        this.repository = getRepository(Users);
+    }
+
+    public show = async (id: number): Promise<Users> => this.findOne(id);
+
+    public store = async (data: Users): Promise<Users> => this.repository.save(data);
+
+    public updateData = async(data: Users): Promise<UpdateResult> => this.repository.update(data.id, data);
+
+    public removeData = async (id: number): Promise<DeleteResult> => this.delete(id);
+}
