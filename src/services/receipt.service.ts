2375ce0fd5a6028cb86eda8ddc07e61e1e771568
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-22 20:50:19 -0300

    chore: add new receipt service

---
+++ src/services/receipt.service.ts
@@ -0,0 +1,33 @@
+import Receipt from "../models/receipt";
+import ReceiptRepository from "../repository/receipt.repository";
+
+export default class ReceiptService {
+    private repository: ReceiptRepository;
+
+    constructor() {
+        this.repository = new ReceiptRepository();
+    }
+
+    public show = async (id: number): Promise<Receipt> => this.repository.show(id);
+
+    public listByIdUser = async (id_user: number): Promise<Receipt[]> =>
+        this.repository.listByIdUser(id_user);
+
+    public store = async (data: Receipt): Promise<Receipt> => this.repository.store(data);
+
+    public updateData = async (data: Receipt): Promise<any> => {
+        const obj = await this.repository.update(data.id, data);
+        console.log(obj);
+        return obj;
+    }
+
+    public removeData = async (id: number): Promise<string> => {
+        const obj = await this.show(id);
+        if (obj) {
+            await this.repository.delete(obj.id);
+            return 'receipt deleted.';
+        }
+        return 'receipt not found.';
+    }
+
+}
