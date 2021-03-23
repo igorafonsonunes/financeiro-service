8a044912f7db39edb4f4f52e862c7047a18e5667
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-22 22:47:54 -0300

    chore: add swaggerDef

---
+++ swaggerDef.js
@@ -0,0 +1,29 @@
+module.exports = {
+    info: {
+      title: 'Console API',
+      version: '1.0.0',
+      description: 'A sample API',
+    },
+    openapi: '3.0.0',
+    servers: [
+      {
+        "url": "http://localhost:3000"
+      },
+    ],
+    apis: ['src/controllers/*.ts'],
+    components: {
+      securitySchemes: {
+        ApiKeyAuth: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'authorization',
+        },
+        XApiKeyAuth: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'x-api-key',
+        },
+      },
+    },
+  };
+  
