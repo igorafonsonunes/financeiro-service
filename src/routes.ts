d5c047e4253a218b8cb1d382230b8a723e89d59b
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-04-02 18:44:30 -0300

    chore: add new router Receipt.

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import ReceiptRoutes from './routers/receipt.routes';
 import UsersRoutes from './routers/users.routes';
 
 export default class Routes{
@@ -10,6 +11,7 @@ export default class Routes{
 
     public init = (): Router =>{
         this.router.use(new UsersRoutes().listRoutes());
+        this.router.use(new ReceiptRoutes().listRoutes());
         return this.router;
     }
 }
